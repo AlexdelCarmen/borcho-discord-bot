b8a0433153e5b42d58db49c5f6bc1ababfbb9654
Borcho <alexbdelcarmen@gmail.com>
2026-02-14 01:04:54 -0600

    Add embed to ping command and update color

--- cogs/ping.py
+++ cogs/ping.py
@@ -7,7 +7,14 @@ class Ping(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx): 
-        await ctx.send("!Pong")
+        embed = discord.Embed(
+            title="🏓 Pong!",
+            description="BorchoBot te saluda",
+            color=0xBFD7EA
+        )
+        latency = round(self.bot.latency * 1000)
+        embed.add_field(name="Latencia", value=f"{latency}ms", inline=False)
+        await ctx.send(embed=embed)
 
 async def setup(bot): 
     await bot.add_cog(Ping(bot))
